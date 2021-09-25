0baab5ae1d962a332a9e224e806b0ab663d7d588
Sadman95 <mail.sakib.1995@gmail.com>
2021-09-26 02:16:13 +0600

    header added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1 className="main-title"><span>Tv</span> Flix</h1>
+            <h5>Your Favorite Place For Tv Series</h5>
+        </div>
+    );
+};
+
+export default Header;
